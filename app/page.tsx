a4826465e076a77853bf80b0ad133286ffbe14be
Luis Martinez <mrluismartinezzz@gmail.com>
2024-04-20 10:53:40 -0700

    organized btns next to their corresponding tables

--- app/page.tsx
+++ app/page.tsx
@@ -28,43 +28,43 @@ export default function Home() {
     setRunningRows([...runningRows, { interval: '', distance: '', time: '', difficulty: '' }]);
   };
 
-  const handleWeightliftingWeightChange = (value, index) => {
+  const handleWeightliftingWeightChange = (value: string, index: number) => {
     const newRows = [...weightliftingRows];
     newRows[index].weight = value;
     setWeightliftingRows(newRows);
   };
 
-  const handleWeightliftingRepsChange = (value, index) => {
+  const handleWeightliftingRepsChange = (value: string, index: number) => {
     const newRows = [...weightliftingRows];
     newRows[index].reps = value;
     setWeightliftingRows(newRows);
   };
 
-  const handleWeightliftingDifficultyChange = (value, index) => {
+  const handleWeightliftingDifficultyChange = (value: string, index: number) => {
     const newRows = [...weightliftingRows];
     newRows[index].difficulty = value;
     setWeightliftingRows(newRows);
   };
 
-  const handleRunningIntervalChange = (value, index) => {
+  const handleRunningIntervalChange = (value: string, index: string | number) => {
     const newRows = [...runningRows];
-    newRows[index].interval = value;
+    newRows[Number(index)].interval = value;
     setRunningRows(newRows);
   };
 
-  const handleRunningDistanceChange = (value, index) => {
+  const handleRunningDistanceChange = (value: string, index: number) => {
     const newRows = [...runningRows];
     newRows[index].distance = value;
     setRunningRows(newRows);
   };
 
-  const handleRunningTimeChange = (value, index) => {
+  const handleRunningTimeChange = (value: string, index: number) => {
     const newRows = [...runningRows];
     newRows[index].time = value;
     setRunningRows(newRows);
   };
 
-  const handleRunningDifficultyChange = (value, index) => {
+  const handleRunningDifficultyChange = (value: string, index: number) => {
     const newRows = [...runningRows];
     newRows[index].difficulty = value;
     setRunningRows(newRows);
@@ -90,57 +90,62 @@ export default function Home() {
 
   return (
     <main>
-      <h1>hi</h1>
-      <button id = "button" onClick={handleWeightliftingButtonClick}>Weightlifting</button>
-      <button id = "button" onClick={handleRunningButtonClick}>Running</button>
+      <button id="button" onClick={handleWeightliftingButtonClick}>Weightlifting</button>
 
       {showWeightliftingTable && (
-        <Table>
-          <TableCaption>WEIGHTLIFTING WORKOUT SUMMARY!!\n ideally this is where the AI will interpret the results based off sthe user inputs</TableCaption>
-          <TableHeader className = "THeader">
-            <TableRow>
-              <TableHead >Set</TableHead>
-              <TableHead>Weight</TableHead>
-              <TableHead>Reps</TableHead>
-              <TableHead className="text-right">Difficulty</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {weightliftingRows.map((row, index) => (
-              <TableRow id = "genRow" key={index}>
-                <TableCell className="font-medium">{index + 1}</TableCell>
-                <TableCell><input type="text" value={row.weight} onChange={(e) => handleWeightliftingWeightChange(e.target.value, index)} /></TableCell>
-                <TableCell><input type="text" value={row.reps} onChange={(e) => handleWeightliftingRepsChange(e.target.value, index)} /></TableCell>
-                <TableCell className="text-right">
-                  <select value={row.difficulty} onChange={(e) => handleWeightliftingDifficultyChange(e.target.value, index)}>
-                    <option value="">Select</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                  </select>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      )}
+        <div>
 
+          <Table>
+            <TableCaption>WEIGHTLIFTING WORKOUT SUMMARY!!\n ideally this is where the AI will interpret the results based offs the user inputs</TableCaption>
+            <TableHeader className="THeader">
+              <TableRow>
+                <TableHead >Set</TableHead>
+                <TableHead>Weight (lbs)</TableHead>
+                <TableHead>Reps</TableHead>
+                <TableHead className="text-right">Difficulty</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {weightliftingRows.map((row, index) => (
+                <TableRow id="genRow" key={index}>
+                  <TableCell className="font-medium">{index + 1}</TableCell>
+                  <TableCell><input type="text" value={row.weight} onChange={(e) => handleWeightliftingWeightChange(e.target.value, index)} /></TableCell>
+                  <TableCell><input type="text" value={row.reps} onChange={(e) => handleWeightliftingRepsChange(e.target.value, index)} /></TableCell>
+                  <TableCell className="text-right">
+                    <select value={row.difficulty} onChange={(e) => handleWeightliftingDifficultyChange(e.target.value, index)}>
+                      <option value="">Select</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          {showWeightliftingTable && <button id="button" onClick={addWeightliftingRow}>Add Weightlifting Row</button>}
+          {showWeightliftingTable && <button id="button" onClick={generateWeightliftingJsonData}>Display Weightlifting JSON</button>}
+          {weightliftingJsonData && <><button id="button" onClick={clearWeightliftingJsonData}>Clear Weightlifting JSON</button><pre>{weightliftingJsonData}</pre></>}
+        </div>
+      )}
+      <br></br>
+      <button id="button" onClick={handleRunningButtonClick}>Running</button>
       {showRunningTable && (
         <Table>
           <TableCaption>RUNNING WORKOUT SUMMARY!!\n ideally this is where the AI will interpret the results based off sthe user inputs</TableCaption>
           <TableHeader className='THeader'>
             <TableRow>
               <TableHead className="w-[100px]">Interval</TableHead>
-              <TableHead>Distance</TableHead>
-              <TableHead>Time</TableHead>
+              <TableHead>Distance (miles)</TableHead>
+              <TableHead>Time (mins)</TableHead>
               <TableHead className="text-right">Difficulty</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {runningRows.map((row, index) => (
-              <TableRow id = "genRow" key={index}>
+              <TableRow id="genRow" key={index}>
                 <TableCell className="font-medium">{index + 1}</TableCell>
                 <TableCell><input type="text" value={row.distance} onChange={(e) => handleRunningDistanceChange(e.target.value, index)} /></TableCell>
                 <TableCell><input type="text" value={row.time} onChange={(e) => handleRunningTimeChange(e.target.value, index)} /></TableCell>
@@ -160,12 +165,9 @@ export default function Home() {
         </Table>
       )}
 
-      {showWeightliftingTable && <button id = "button" onClick={addWeightliftingRow}>Add Weightlifting Row</button>}
-      {showRunningTable && <button id = "button" onClick={addRunningRow}>Add Running Row</button>}
-      {showWeightliftingTable && <button id = "button" onClick={generateWeightliftingJsonData}>Display Weightlifting JSON</button>}
-      {showRunningTable && <button id = "button" onClick={generateRunningJsonData}>Display Running JSON</button>}
-      {weightliftingJsonData && <><button id = "button" onClick={clearWeightliftingJsonData}>Clear Weightlifting JSON</button><pre>{weightliftingJsonData}</pre></>}
-      {runningJsonData && <><button id = "button" onClick={clearRunningJsonData}>Clear Running JSON</button><pre>{runningJsonData}</pre></>}
+      {showRunningTable && <button id="button" onClick={addRunningRow}>Add Running Row</button>}
+      {showRunningTable && <button id="button" onClick={generateRunningJsonData}>Display Running JSON</button>}
+      {runningJsonData && <><button id="button" onClick={clearRunningJsonData}>Clear Running JSON</button><pre>{runningJsonData}</pre></>}
     </main>
   );
 }
